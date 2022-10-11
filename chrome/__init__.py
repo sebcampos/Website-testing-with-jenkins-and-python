e8811033ed2a24d01e2964f2ad4d394d88b0a6ef
sebastiancampos <sebcampos@criptext.com>
2022-10-11 13:41:41 -0700

    created dummy test cases to test with jenkins

--- chrome/__init__.py
+++ chrome/__init__.py
@@ -4,12 +4,13 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 
 
-def init_webdriver(headless=None):
+def init_webdriver(headless=None, production=False):
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=1920,1080")
     if headless:
         options.headless = True
-        driver = webdriver.Chrome(options=options)  # webdriver.Chrome(options=options)  # run headless mode
+    if production:
+        driver = webdriver.Chrome(options=options, executable_path="/home/thesensisocietydelivery/google-chrome-stable_current_amd64.deb")
     else:
         driver = webdriver.Chrome(options=options)
     driver.maximize_window()
