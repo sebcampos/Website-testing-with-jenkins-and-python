70ce4b4574cdc60233081f1c719b42902f06aff5
sebastiancampos <sebcampos@criptext.com>
2022-10-15 16:56:11 -0700

    debugging

--- test_API_endpoint_cases.py
+++ test_API_endpoint_cases.py
@@ -1,10 +1,11 @@
 import requests
+from Webpages import utils
 
-admin_url= 'https://thesensisociety.com/services/automation/admin/'
-admin_post_url= 'https://thesensisociety.com/services/automation/admin/login/?next=/admin/'
+admin_url = 'https://thesensisociety.com/services/automation/admin/'
+admin_post_url = 'https://thesensisociety.com/services/automation/admin/login/?next=/admin/'
 
-UN='Sensi'
-PWD='1a7428Sensi420'
+UN = 'Sensi'
+PWD = '1a7428Sensi420'
 client = requests.session()
 
 # Retrieve the CSRF token first
@@ -14,13 +15,18 @@ login_data = dict(username=UN, password=PWD, csrfmiddlewaretoken=csrftoken)
 r = client.post(admin_post_url, data=login_data, headers={"Referer": "foo"}, verify=False)
 headers = {"Referer": "foo", "X-csrftoken": csrftoken}
 
+
 def teardown_module(module):
     """ teardown any state that was previously setup with a setup_module
     method.
     """
+    global api_logger
+    api_logger = utils.set_up_logger("api_logger")
     client.close()
 
-def post_test():
-    client.post("https://thesensisociety.com/services/apiv1/qrcode_endpoint/", data={"TEST_POST"}, headers=headers)
 
+def post_test():
+    r = client.post("https://thesensisociety.com/services/apiv1/qrcode_endpoint/", data={"TEST_POST": 1}, headers=headers)
+    api_logger.info(f"{r.content}")
+    assert r.status_code == 200
 
